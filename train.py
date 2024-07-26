3d1cf0dd15f3ec7a0e875ae70daa93ef134c693f
119176 <1191767612@qq.com>
2024-07-26 13:38:33 +0800

    dota_to_yolo

--- train.py
+++ train.py
@@ -3,16 +3,16 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
+    model = RTDETR('/home/class1/work/zhangnan/RTDETR-master/ultralytics/cfg/models/rt-detr/rtdetr-DCNV2-Dynamic.yaml')
     # model.load('') # loading pretrain weights
-    model.train(data='dataset/data.yaml',
+    model.train(data='/home/class1/work/zhangnan/RTDETR-master/dataset/data.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=100,
-                batch=4,
+                batch=8,
                 workers=4,
                 device='0',
                 # resume='', # last.pt path
                 project='runs/train',
-                name='exp',
+                name='DCNV2-Dynamic',
                 )
